100ce5cb013de35e6d51717aabf19cb2766f6106
ire4564 <ire4564@gmail.com>
2021-05-31 21:08:14 +0900

    Add Parper to Avator

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import Avatar from './component/Avatar';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>connect Paper Page!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+
+          <Avatar></Avatar>
+          <Text>23 years old</Text>
+          <Text>MIN PARK</Text>
+
+        </View>
+    </PaperProvider>
   );
 }
 
---
+++ component/Avatar.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Avatar } from 'react-native-paper';
+
+const Avatar_component = () => (
+  <Avatar.Image size={70} source={require('../assets/profile.jpg')} />
+);
+export default Avatar_component;
